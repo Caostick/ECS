9f838aad5872f420e610410aac6e0951cafbe72b
Caostick <alexandr_gricenko@mail.ru>
2026-01-21 00:36:42 +0300

    Remove graphical example

--- Examples/DemoScene/Utils.cpp
+++
@@ -1,7 +0,0 @@
-#include "Utils.h"
-
-#include <stdlib.h>
-
-auto RndRange(float min, float max) -> float {
-	return ((rand() % 10000) / 10000.0f) * (max - min) + min;
-}
--- Examples/DemoScene/Utils.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-auto RndRange(float min, float max) -> float;
